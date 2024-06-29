0e8693db7c0dccfc94e0a15c0ae7c453bfd71dda
Matheus Medeiros <0000108202241XSP@al.educacao.sp.gov.br>
2024-06-29 09:13:44 -0300

    Criado Classe ConversorTemperatura

---
+++ calculadora_temp.py
@@ -0,0 +1,9 @@
+class ConversorTemperatura:
+    def __init__():
+        pass
+
+    def celsius_to_fahrenheit(celsius_value):
+        return celsius_value * (9/5) + 32
+    
+    def fahrenheit_to_celsius(fah_value):
+         return (fah_value - 32) / (9/5)
